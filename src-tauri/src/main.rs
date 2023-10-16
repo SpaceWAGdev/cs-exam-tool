8acba7bfc6e5c9a29f97ea8a50d700bb44c9dd35
starlight_caffeine <starlight-caffeine@posteo.org>
2023-10-17 00:22:33 +0200

    Add Program State Manager

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,7 +4,13 @@ mod permissions;
 mod processes;
 
 use std::vec;
+use lazy_static::lazy_static;
+use std::sync::Mutex;
 
+lazy_static! {
+    static ref PROGRAM_STATE: Mutex<i32> = Mutex::new(0);
+}
+    
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -21,6 +27,11 @@ fn get_process_list() -> Vec<String> {
    processes::list_all_processes()
 }
 
+#[tauri::command]
+fn get_program_state() -> i32 {
+    *PROGRAM_STATE.lock().unwrap()
+}
+
 fn main() {
     println!("Is Admin: {}", permissions::elevated());
 
@@ -28,4 +39,5 @@ fn main() {
         .invoke_handler(tauri::generate_handler![greet, get_admin_permissions, get_process_list])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+
 }
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,8 @@
 import { invoke } from "@tauri-apps/api/tauri";
+import { emit, listen } from "@tauri-apps/api/event";
 
-let greetInputEl: HTMLInputElement | null;
-let greetMsgEl: HTMLElement | null;
 
-async function greet() {
-  if (greetMsgEl && greetInputEl) {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    greetMsgEl.textContent = await invoke("greet", {
-      name: greetInputEl.value,
-    });
-  }
-}
 window.addEventListener("DOMContentLoaded", async () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form")?.addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
-  });
-
   let is_elevated : Boolean = await invoke("get_admin_permissions");
   document.querySelector("#admin_rights")!.innerHTML = is_elevated == true? "Admin" : "Normal User";
 
